37a224c5ef5f68040f881a391c30ddfc18502a3b
Lucas Yang <lucasmf@yahoo-inc.com>
2016-06-23 21:39:48 +0800

    fix errors

--- chef_setup_server/cookbooks/users/providers/manage.rb
+++ chef_setup_server/cookbooks/users/providers/manage.rb
@@ -56,7 +56,9 @@ action :create do
   else
     puts new_resource.inspect
     puts new_resource.data_bag.inspect
+    puts new_resource.search_group.inspect
     search(new_resource.data_bag, "groups:#{new_resource.search_group} AND NOT action:remove") do |u|
+      puts u.inspect
       u['username'] ||= u['id']
       u['groups'].each do |g|
         users_groups[g] = [] unless users_groups.key?(g)
